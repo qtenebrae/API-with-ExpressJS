d756b9e10e0f36b905428810f01d5b834ef8f857
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-17 20:24:32 +0300

    Getting Started Developing an Application Architecture
    Layer architecture is used
    Application class is implemented
    Added logger
    Controller base class and user controller created

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Express } from "express";
+import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
+
+export class App {
+  app: Express;
+  server: Server;
+  port: number;
+  logger: LoggerService;
+  userController: UserController;
+
+  constructor(logger: LoggerService, userController: UserController) {
+    this.app = express();
+    this.port = 8000;
+    this.logger = logger;
+    this.userController = userController;
+  }
+
+  useRoutes() {
+    this.app.use("/users", this.userController.router);
+  }
+
+  public async init() {
+    this.useRoutes();
+    this.server = this.app.listen(this.port);
+    this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { App } from "./app";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
+
+async function bootstrap() {
+  const logger = new LoggerService();
+  const app = new App(logger, new UserController(logger));
+  await app.init();
+}
+
+bootstrap();
