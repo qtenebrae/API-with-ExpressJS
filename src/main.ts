50871047c7c19ec7190460a33c7575dfbb11e30c
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-18 22:02:16 +0300

    Created IUserController interface
    The UserController class implements this interface

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Container } from "inversify";
+import { Container, ContainerModule, interfaces } from "inversify";
 import { App } from "./app";
 import { TYPES } from "./types";
 import { ILogger } from "./logger/logger.interface";
@@ -6,13 +6,21 @@ import { LoggerService } from "./logger/logger.service";
 import { IExeptionFilter } from "./errors/exeption.filter.interface";
 import { ExeptionFilter } from "./errors/exeption.filter";
 import { UserController } from "./users/users.controller";
+import { IUserController } from "./users/users.controller.interface";
 
-const appContainer = new Container();
-appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
-appContainer.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-appContainer.bind<UserController>(TYPES.UserController).to(UserController);
-appContainer.bind<App>(TYPES.Application).to(App);
-const app = appContainer.get<App>(TYPES.Application);
-app.init();
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+  bind<ILogger>(TYPES.ILogger).to(LoggerService);
+  bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+  bind<IUserController>(TYPES.UserController).to(UserController);
+  bind<App>(TYPES.Application).to(App);
+});
 
-export { app, appContainer };
+function bootstrap() {
+  const appContainer = new Container();
+  appContainer.load(appBindings);
+  const app = appContainer.get<App>(TYPES.Application);
+  app.init();
+  return { appContainer, app };
+}
+
+export const { app, appContainer } = bootstrap();
