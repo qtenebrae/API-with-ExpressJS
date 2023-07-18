6ff9cf6de7c5ef6e103470e81e9bf81f5450bc80
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-18 21:48:37 +0300

    Dependency Injection pattern implemented
    IoC container implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller";
-import { LoggerService } from "../logger/logger.service";
 import { HTTPError } from "../errors/http-error.class";
+import { ILogger } from "../logger/logger.interface";
+import { TYPES } from "../types";
+import { inject, injectable } from "inversify";
+import "reflect-metadata";
 
+@injectable()
 export class UserController extends BaseController {
-  constructor(logger: LoggerService) {
-    super(logger);
+  constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+    super(loggerService);
     this.bindRoutes([
       { path: "/register", method: "post", func: this.register },
     ]);
