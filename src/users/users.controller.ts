adb0b5a1c48fa74ef68573e16ae60cb2bc8c228e
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-19 16:43:41 +0300

    Getting started with DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,21 +6,26 @@ import { TYPES } from '../types';
 import { inject, injectable } from 'inversify';
 import 'reflect-metadata';
 import { IUserController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
 		super(loggerService);
-		this.bindRoutes([{ path: '/register', method: 'post', func: this.register }]);
-		this.bindRoutes([{ path: '/login', method: 'post', func: this.login }]);
+		this.bindRoutes([
+			{ path: '/register', method: 'post', func: this.register },
+			{ path: '/login', method: 'post', func: this.login },
+		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
-		// this.ok(res, "login");
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		next(new HTTPError(401, 'Ошибка авторизации', 'login'));
 	}
 
-	register(req: Request, res: Response, next: NextFunction): void {
+	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		this.ok(res, 'register');
 	}
 }
