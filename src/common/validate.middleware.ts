ce66747370401e3fe2aec707cec93482cbdc36ee
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-21 15:10:15 +0300

    Added middleware for routes and data validation

---
+++ src/common/validate.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { IMiddleWare } from './middleware.interface';
+import { ClassConstructor, plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+
+export class ValidateMiddleware implements IMiddleWare {
+	constructor(private classToValidate: ClassConstructor<object>) {}
+
+	execute({ body }: Request, res: Response, next: NextFunction): void {
+		const instance = plainToClass(this.classToValidate, body);
+		validate(instance).then((errors) => {
+			if (errors.length > 0) {
+				res.status(422).send(errors);
+			} else {
+				next();
+			}
+		});
+	}
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { IUserService } from './users.service.interface';
+import { ValidateMiddleware } from '../common/validate.middleware';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -18,7 +19,12 @@ export class UserController extends BaseController implements IUserController {
 	) {
 		super(loggerService);
 		this.bindRoutes([
-			{ path: '/register', method: 'post', func: this.register },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
 			{ path: '/login', method: 'post', func: this.login },
 		]);
 	}
