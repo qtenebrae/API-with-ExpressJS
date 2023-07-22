eb30205fa5fc6891fd98d105ec58c8604ddabe6f
qtenebrae <serega.ivanovv2012@gmail.com>
2023-07-22 16:30:40 +0300

    Add .env file and configured to work with it

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { UserController } from './users/users.controller';
 import { IUserController } from './users/users.controller.interface';
 import { UserService } from './users/users.service';
 import { IUserService } from './users/users.service.interface';
+import { IConfigService } from './config/config.service,interface';
+import { ConfigService } from './config/config.service';
 
 export interface IBootstrapReturn {
 	appContainer: Container;
@@ -16,10 +18,11 @@ export interface IBootstrapReturn {
 }
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-	bind<ILogger>(TYPES.ILogger).to(LoggerService);
-	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-	bind<IUserController>(TYPES.UserController).to(UserController);
-	bind<IUserService>(TYPES.UserService).to(UserService);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter).inSingletonScope();
+	bind<IUserController>(TYPES.UserController).to(UserController).inSingletonScope();
+	bind<IUserService>(TYPES.UserService).to(UserService).inSingletonScope();
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,18 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { User } from './user.entity';
 import { IUserService } from './users.service.interface';
+import { IConfigService } from '../config/config.service,interface';
+import { TYPES } from '../types';
 
 @injectable()
 export class UserService implements IUserService {
+	constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
 	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
 		const newUser = new User(email, name);
-		await newUser.setPassword(password);
+		const salt = this.configService.get('SALT');
+		await newUser.setPassword(password, Number(salt));
 		// Провека, что такое пользователь уже есть
 		// Если существует - возвращаем null, иначе создаем
 		return null;
